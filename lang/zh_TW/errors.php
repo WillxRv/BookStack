f8c0bef434c62cf911703e67fa53690638a9d14e
Dan Brown <email@danb.me>
2024-12-01 13:15:20 +0000

    New translations errors.php (Chinese Traditional)

--- lang/zh_TW/errors.php
+++ lang/zh_TW/errors.php
@@ -105,6 +105,18 @@ return [
     'app_down' => ':appName 離線中',
     'back_soon' => '它應該很快就會重新上線。',
 
+    // Import
+    'import_zip_cant_read' => 'Could not read ZIP file.',
+    'import_zip_cant_decode_data' => 'Could not find and decode ZIP data.json content.',
+    'import_zip_no_data' => 'ZIP file data has no expected book, chapter or page content.',
+    'import_validation_failed' => 'Import ZIP failed to validate with errors:',
+    'import_zip_failed_notification' => 'Failed to import ZIP file.',
+    'import_perms_books' => 'You are lacking the required permissions to create books.',
+    'import_perms_chapters' => 'You are lacking the required permissions to create chapters.',
+    'import_perms_pages' => 'You are lacking the required permissions to create pages.',
+    'import_perms_images' => 'You are lacking the required permissions to create images.',
+    'import_perms_attachments' => 'You are lacking the required permission to create attachments.',
+
     // API errors
     'api_no_authorization_found' => '在請求上找不到授權權杖',
     'api_bad_authorization_format' => '在請求中找到授權權杖，但格式似乎不正確',
