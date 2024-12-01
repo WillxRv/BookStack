cbff2b6b12ce817bca1987f285760186e4023900
Dan Brown <email@danb.me>
2024-12-01 13:13:43 +0000

    New translations activities.php (Chinese Traditional)

--- lang/zh_TW/activities.php
+++ lang/zh_TW/activities.php
@@ -84,6 +84,14 @@ return [
     'webhook_delete' => 'webhook 已刪除',
     'webhook_delete_notification' => 'Webhook 已刪除成功',
 
+    // Imports
+    'import_create' => 'created import',
+    'import_create_notification' => 'Import successfully uploaded',
+    'import_run' => 'updated import',
+    'import_run_notification' => 'Content successfully imported',
+    'import_delete' => 'deleted import',
+    'import_delete_notification' => 'Import successfully deleted',
+
     // Users
     'user_create' => '建立使用者',
     'user_create_notification' => '使用者已成功建立。',
