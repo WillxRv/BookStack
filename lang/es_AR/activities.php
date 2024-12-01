4397bf319dea2ae7d85539f70891688ca001d161
Dan Brown <email@danb.me>
2024-12-01 13:13:49 +0000

    New translations activities.php (Spanish, Argentina)

--- lang/es_AR/activities.php
+++ lang/es_AR/activities.php
@@ -84,6 +84,14 @@ return [
     'webhook_delete' => 'webhook eliminado',
     'webhook_delete_notification' => 'Webhook eliminado correctamente',
 
+    // Imports
+    'import_create' => 'created import',
+    'import_create_notification' => 'Import successfully uploaded',
+    'import_run' => 'updated import',
+    'import_run_notification' => 'Content successfully imported',
+    'import_delete' => 'deleted import',
+    'import_delete_notification' => 'Import successfully deleted',
+
     // Users
     'user_create' => 'usuario creado',
     'user_create_notification' => 'Usuario creado correctamente',
