473a3802171dca03799a3b0d8079f739755a8b7e
Dan Brown <email@danb.me>
2024-12-01 13:13:23 +0000

    New translations activities.php (Greek)

--- lang/el/activities.php
+++ lang/el/activities.php
@@ -84,6 +84,14 @@ return [
     'webhook_delete' => 'διαγραμμένο webhook',
     'webhook_delete_notification' => 'Το Webhook διαγράφηκε επιτυχώς',
 
+    // Imports
+    'import_create' => 'created import',
+    'import_create_notification' => 'Import successfully uploaded',
+    'import_run' => 'updated import',
+    'import_run_notification' => 'Content successfully imported',
+    'import_delete' => 'deleted import',
+    'import_delete_notification' => 'Import successfully deleted',
+
     // Users
     'user_create' => 'δημιουργημένος χρήστης',
     'user_create_notification' => 'Ο χρήστης δημιουργήθηκε με επιτυχία',
